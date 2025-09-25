a413fe6f5ec4187fe547471e74af12d2b7a3cca8
Robson-Lopes-Jr <202508571391@alunos.estacio.br>
2025-09-25 02:58:54 +0000

    batalhaNaval, nível: aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,41 @@
 #include <stdio.h>
 
+#define linhas 10
+#define colunas 10
+
 int main (){
 
-    int i, j;
+    int matriz [linhas][colunas] = {0};
     int linha = 0, coluna = 0, tamanho = 3;
     int linha1 = 7, coluna1 = 9, tamanho1 = 3;
+    int linha2 = 0, coluna2 = 5, tamanho2 = 3;
+    int linha3 = 2, coluna3 = 4, tamanho3 = 3;
 
     printf("Tabuleiro Batalha Naval\n");
-    int matriz [10][10] = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-    };
 
-    //posição do navio 1
-    for (j = coluna; j < coluna + tamanho; j++)
-    {
+    // posição do navio horizontal
+    for (int j = coluna; j < coluna + tamanho; j++) {
         matriz[linha][j] = 3;
     }
     
-    //posição do navio 2
-    for (i = linha1; i < linha1 + tamanho1; i++)
-    {
+    // posição do navio vertical
+    for (int i = linha1; i < linha1 + tamanho1; i++) {
         matriz[i][coluna1] = 3;
     }
     
-    //imprimindo o tabuleiro
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
+    // posição do 1º navio diagonal
+    for (int k = 0; k < tamanho2; k++) {
+        matriz[linha2 + k][coluna2 + k] = 3;
+    }
+
+    // posição do 2º navio diagonal
+    for (int k = 0; k < tamanho3; k++) {
+        matriz[linha3 + k][coluna3 - k] = 3;
+    }
+
+    // imprimindo o tabuleiro
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
             printf("%d ", matriz[i][j]);
         }
         printf("\n");
